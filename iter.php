47e9214ab8fa9fc680bd28d8cc17a972a32cc903
Umang Bhatt <bhatt.umang7@gmail.com>
2013-05-24 17:28:24 +0530

    removed un used code

--- iter.php
+++ iter.php
@@ -2,7 +2,6 @@
 <html>
 
     <h1>Search In Content</h1>
-<?php var_dump(isset($_POST['action']) && $_POST['action'] == 'content'); ?>
 <br>
 
 
@@ -11,17 +10,17 @@
 
 	<form method="post" action="iter.php">
 		<label for="content_1">File with content</label> <input  type="radio" name="action" id="content_1"
-			value="content" <?php echo  isset($_POST['action']) && $_POST['action'] == 'content' ? ' checked="checked"   ' : '' ; ?> > 
+			value="content"  > 
 			
 			<br> 
 			
 			<label for="file_name_1">File path with given name</label> <input
-			type="radio" name="action" id="file_name_1" value="file_name" <?php echo isset($_POST['action']) && $_POST['action'] == 'file_name' ? " checked='checked' " : '' ; ?> > 
+			type="radio" name="action" id="file_name_1" value="file_name"  > 
 			
 			<br /> Content: 
 			
 			<input
-			type="text" value="" name="value" value="<?php echo isset($_POST['value']) && $_POST['value'] ? $_POST['value'] : ''; ?>" > <br> <input type="submit">
+			type="text" value="" name="value"  > <br> <input type="submit">
 
 	</form>
 </table>
